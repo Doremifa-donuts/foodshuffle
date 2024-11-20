3d722af0be9bac0a6ce38ab6928aa17a2fa2c5c3
haruna1256 <haru_kawagishi@icloud.com>
2024-11-20 09:35:43 +0900

    アニメーション　テスト

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -30,7 +30,7 @@ class Footer extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const ReviewPage()),
+                  blackOut(const ReviewPage()),
                 );
               }),
           _FooterIcon(
@@ -40,7 +40,7 @@ class Footer extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const GroupPage()),
+                  flipTransition(const GroupPage()),
                 );
               }),
           _FooterIcon(
@@ -60,7 +60,7 @@ class Footer extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const ArchivePage()),
+                  diagonalSlideTransition(const ArchivePage()),
                 );
               }),
           _FooterIcon(
@@ -70,7 +70,7 @@ class Footer extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const MyPage()),
+                  verticalFlipTransition(const MyPage()),
                 );
               }),
         ],
@@ -126,4 +126,114 @@ class _FooterIcon extends StatelessWidget {
       ),
     );
   }
+}
+
+// 画面遷移アニメーション　blackOut
+PageRouteBuilder<Object?> blackOut(Widget screen) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => screen,
+    transitionDuration: const Duration(seconds: 1),
+    reverseTransitionDuration: const Duration(seconds: 1),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      final color = ColorTween(
+        begin: Colors.transparent,
+        end: Colors.black,
+      ).animate(
+        CurvedAnimation(
+          parent: animation,
+          curve: const Interval(0, 0.5, curve: Curves.easeInOut),
+        ),
+      );
+      final opacity = Tween<double>(
+        begin: 0,
+        end: 1,
+      ).animate(
+        CurvedAnimation(
+          parent: animation,
+          curve: const Interval(0.5, 1, curve: Curves.easeInOut),
+        ),
+      );
+      return AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return Container(
+            color: color.value,
+            child: Opacity(
+              opacity: opacity.value,
+              child: child,
+            ),
+          );
+        },
+        child: child,
+      );
+    },
+  );
+}
+
+// 画面遷移アニメーション　flip
+PageRouteBuilder<Object?> flipTransition(Widget screen) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => screen,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          final flipAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+            CurvedAnimation(parent: animation, curve: Curves.linear),
+          );
+          return Transform(
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.001)
+              ..rotateY(2 * 3.14 * flipAnimation.value),
+            alignment: Alignment.center,
+            child: child,
+          );
+        },
+        child: child,
+      );
+    },
+  );
+}
+
+// 画面遷移アニメーション　diagonal
+PageRouteBuilder<Object?> diagonalSlideTransition(Widget screen) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => screen,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      final offsetAnimation = Tween<Offset>(
+        begin: const Offset(-1.0, -1.0),
+        end: Offset.zero,
+      ).animate(animation);
+
+      return SlideTransition(
+        position: offsetAnimation,
+        child: child,
+      );
+    },
+  );
+}
+
+// 画面遷移アニメーション　flip
+PageRouteBuilder<Object?> verticalFlipTransition(Widget screen) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => screen,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      final flipAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(parent: animation, curve: Curves.linear),
+      );
+      return AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.001)
+              ..rotateX(2 * 3.14 * flipAnimation.value),
+            child: child,
+          );
+        },
+        child: child,
+      );
+    },
+  );
 }
