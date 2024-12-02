b86dcc1114715211377d9be0025b26d592c5e4ff
haruna1256 <haru_kawagishi@icloud.com>
2024-12-02 18:59:45 +0900

    add QRコード画面

--- lib/screens/qr_code.dart
+++ lib/screens/qr_code.dart
@@ -1,83 +1,117 @@
-import 'dart:developer';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'dart:developer'; // ログ出力用
+import 'dart:io'; // プラットフォーム判別用 (Android, iOSの確認)
+import 'package:qr_code_scanner/qr_code_scanner.dart'; // QRコードスキャナー用ライブラリ
 
+import '../widgets/footer.dart'; // フッターを表示するウィジェット
+import '../model/color.dart'; // カラーパス
+
+// 状態管理のためのステートを作成
 class QrScanView extends StatefulWidget {
-  const QrScanView({super.key});
+  const QrScanView({super.key}); // コンストラクタ
 
   @override
   _QrScanViewState createState() => _QrScanViewState();
 }
 
+// QrScanViewの状態を管理するクラス
 class _QrScanViewState extends State<QrScanView> {
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR'); // QRコードビューを識別するキー
+  QRViewController? controller; // QRコードスキャナーを制御するコントローラー
 
-  // In order to get hot reload to work we need to pause the camera if the platform
-  // is android, or resume the camera if the platform is iOS.
+  // 再描画時のカメラ制御 (ホットリロード対応)
   @override
   void reassemble() {
     super.reassemble();
     if (Platform.isAndroid) {
-      controller?.pauseCamera();
+      controller?.pauseCamera(); // Androidの場合はカメラを一時停止
     }
-    controller?.resumeCamera();
+    controller?.resumeCamera(); // iOSの場合はカメラを再開
   }
 
+  // ウィジェット破棄時の処理 (リソース解放)
   @override
   void dispose() {
-    controller?.dispose();
+    controller?.dispose(); // コントローラーを破棄してリソースを解放
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('QR読み取り'),
-        centerTitle: true,
+        appBar: AppBar(
+          title: const Text('QRコードを読み取ろう!'), // アプリバーのタイトル
+          backgroundColor: const Color(mainColor), // 背景色 (カスタムテーマ)
+          centerTitle: true, // タイトルを中央揃え
+        ),
+        body: Stack(
+          children: [
+            buildBackground(), // 背景画像を構築
+            _buildQrView(context), // QRコードビューを構築
+            const Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Footer(),
+            ),
+          ],
+        ));
+  }
+
+  // 背景画像の構築
+  Widget buildBackground() {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/backimg.jpg'),
+          fit: BoxFit.cover,
+        ),
       ),
-      body: _buildQrView(context),
     );
   }
 
+  // QRコードビューの構築
   Widget _buildQrView(BuildContext context) {
-    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    // デバイスサイズに応じてスキャン領域のサイズを調整
     var scanArea = (MediaQuery.of(context).size.width < 400 ||
             MediaQuery.of(context).size.height < 400)
-        ? 150.0
-        : 300.0;
-    // To ensure the Scanner view is properly sizes after rotation
-    // we need to listen for Flutter SizeChanged notification and update controller
+        ? 150.0 // 小さいデバイスの場合
+        : 300.0; // 通常サイズのデバイスの場合
+
     return QRView(
-      key: qrKey,
-      onQRViewCreated: _onQRViewCreated,
+      key: qrKey, // QRビューを識別するためのキー
+      onQRViewCreated: _onQRViewCreated, // QRビュー作成時のコールバック
       overlay: QrScannerOverlayShape(
-          borderColor: Colors.red,
-          borderRadius: 10,
-          borderLength: 30,
-          borderWidth: 10,
-          cutOutSize: scanArea),
-      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+        // QRコードスキャナーのオーバーレイデザイン
+        borderColor: const Color(mainColor), // 枠の色
+        borderRadius: 10, // 枠の角の丸み
+        borderLength: 30, // 枠の長さ
+        borderWidth: 10, // 枠の幅
+        cutOutSize: scanArea, // スキャン領域のサイズ
+      ),
+      onPermissionSet: (ctrl, p) =>
+          _onPermissionSet(context, ctrl, p), // パーミッションの確認
     );
   }
 
+  // QRビューが作成された際の処理
   void _onQRViewCreated(QRViewController controller) {
     setState(() {
-      this.controller = controller;
+      this.controller = controller; // コントローラーを設定
     });
+    // スキャンされたデータをリアルタイムで取得
     controller.scannedDataStream.listen((scanData) async {
-      print(scanData.code);
+      print(scanData.code); // スキャンしたQRコードのデータをコンソールに出力
     });
   }
 
+  // パーミッションの状態が変化したときの処理
   void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
-    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p'); // ログにパーミッション状態を記録
     if (!p) {
+      // パーミッションが拒否された場合
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('no Permission')),
+        const SnackBar(content: Text('no Permission')), // ユーザーに通知
       );
     }
   }
