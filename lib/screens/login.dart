c96af77b0dd11309721d00b0a5b130b73933554e
Toma <kirima--idea--@au.com>
2024-12-11 15:56:52 +0900

    [update]httpリクエストを送信できるようにhttpモジュールを追加 サーバーとのhttp通信の処理を追加

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 // アニメーションを挟む
 import '../../screens/animation.dart';
 // 画像パスを管理するクラス
 import '../model/images.dart';
+//httpリクエスト用のモジュール
+import 'package:http/http.dart' as http;
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -83,19 +87,80 @@ class _LoginPageState extends State<LoginPage> {
 
                     // ログインボタン
                     ElevatedButton(
-                      onPressed: () {
+                      onPressed: () async {
                         // 入力されたメールアドレスとパスワードを取得
                         final email = _emailController.text;
                         final password = _passwordController.text;
 
                         if (email.isNotEmpty && password.isNotEmpty) {
-                          // ログイン処理成功時に遷移
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const AnimationPage(),
-                            ),
-                          );
+                          // ログイン処理
+                          try {
+                            // HTTPリクエストを送信
+                            final response = await http.post(
+                              Uri.parse('http://127.0.0.1:5678/v1/login'),
+                              headers: {
+                                'Content-Type': 'application/json',
+                                'Accept': 'application/json',
+                                },
+                                // リクエストボディをJSON形式に変換し、emailとpasswordを含める
+                              body: jsonEncode({
+                                'MailAddress': email,
+                                'Password': password,
+                              }),
+                            );
+                            // レスポンスボディをJSON形式に変換 {Response: {Data: {JtiToken:~~~}, Status: ~~~}}
+                            final responseBody = jsonDecode(response.body);;
+                            switch(responseBody['Response']['Status']) {
+                              case 'OK': // ログイン成功(200)
+                                // ログイン処理成功時に遷移
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const AnimationPage(),
+                                  ),
+                                );
+                                break;
+
+                              case 'Bad Request': // ログイン失敗(400)
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text('メールアドレスまたはパスワードが正しくありません。'),
+                                  ),
+                                );
+                                break;
+
+                              case 'Unauthorized': // ログイン失敗(401)
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text('アカウントの認証に失敗しました。'),
+                                  ),
+                                );
+                                break;
+
+                              case 'Internal Server Error': // ログイン失敗(500)
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text('サーバーで問題が発生しました。'),
+                                  ),
+                                );
+                                break;
+
+                              default : //今のところログイン失敗するとerrorのみ返ってくるのでdefaultにたどり着く
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text('メールアドレス、またはパスワードが正しくありません。'),
+                                  ),
+                                );
+                                break;
+                            }
+                          } catch (e) { // 例外処理
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('通信エラー'),
+                              ),
+                            );
+                            print(e);
+                          }
                         } else {
                           // エラーメッセージを表示
                           ScaffoldMessenger.of(context).showSnackBar(
